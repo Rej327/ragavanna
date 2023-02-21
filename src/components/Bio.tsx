3a4e9b61d3ec85ef8db9cc88e91cfb4cd2ac0226
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-21 18:07:48 +0800

    Add bio context with responsive

---
+++ src/components/Bio.tsx
@@ -0,0 +1,16 @@
+export default function Bio() {
+  return (
+    <div className="p-5 bg-stone-200 mx-auto lg:p-10">
+      <h1 className="text-5xl py-5 text-black">Biography</h1>
+      <p className="text-3xl">
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </p>
+      <button className="text-2xl tracking-wider border-2 border-black py-2 my-2 px-20 cursor-pointer font-semibold hover:text-white hover:bg-black">
+        MORE
+      </button>
+    </div>
+  );
+}
---
+++ src/section/Biography.tsx
@@ -0,0 +1,29 @@
+import Donut from "@/app/styleDiv/Donut";
+import Largecircle from "@/app/styleDiv/Largecircle";
+import Stripecircle from "@/app/styleDiv/Stripecircle";
+import Bio from "@/components/Bio";
+import BioImage from "@/components/BioImage";
+
+export default function Biography() {
+  return (
+    <div className="w-full h-full">
+      <div className="grid grid-cols-2 w-full overflow-hidden">
+        <div className="pl-2 mt-10 ml-22 md:mt-15 md:ml-27 lg:mt-20 lg:ml-32">
+          <Stripecircle />
+        </div>
+        <div className="w-fit ml-auto mt-[38rem] md:pr-2">
+          <Largecircle />
+        </div>
+      </div>
+      <div className="-mt-28 w-fit ml-10 sm:mx-auto lg:ml-[67%]">
+        <Donut />
+      </div>
+      <div className="mx-auto my-[-45rem] md:my-[-50rem] p-5 lg:mt-[-48rem] lg:ml-96">
+        <BioImage />
+      </div>
+      <div className="absolute mt-[40rem] px-10 sm:mt-[43rem] lg:w-[40rem] lg:mt-[30rem] ">
+        <Bio />
+      </div>
+    </div>
+  );
+}
