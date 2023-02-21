d70b181f4f1d65c93dac44bad382f3382f1a0734
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-21 18:07:29 +0800

    Add image with responsive

---
+++ src/components/BioImage.tsx
@@ -0,0 +1,11 @@
+export default function BioImage() {
+  return (
+    <div className="w-full">
+      <img
+        src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/b36896b305a65c688f859a79/-min.jpg"
+        alt="Bio Image"
+        className="w-[55rem]"
+      />
+    </div>
+  );
+}
