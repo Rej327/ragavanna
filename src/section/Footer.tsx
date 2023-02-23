c43bb511298b66033ea1e04112ca7bc9299411fc
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-23 17:54:11 +0800

    Add footer section

---
+++ src/section/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="w-full text-center pt-10 bg-[#292e33]">
+      <h1 className="text-stone-400 tracking-widest text-lg">
+        © 2023. ALL RIGHTS RESERVED | Created by Jefferson
+      </h1>
+    </div>
+  );
+}
