880169d8ca7d75a05dc8617b25eff1392de705dc
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-03-03 17:34:42 +0800

    Limit the data and Reversed

--- src/components/LatestEvent.tsx
+++ src/components/LatestEvent.tsx
@@ -28,26 +28,30 @@ export default function LatestEvents() {
     <div className="w-full bg-[#292e33] px-4 py-8">
       <h1 className=" text-white text-4xl md:text-7xl">Category Section</h1>
       {dataState.length <= 0 && <Loader />}
-      {dataState.map((ltsEvent) => (
-        <div className="md:flex px-2 my-4 py-8">
-          <img
-            src={ltsEvent.image}
-            alt="/"
-            className="w-full h-56 md:w-[235px] md:h-[235px] border-2"
-          />
-          <div className="text-white md:px-8 my-auto">
-            <p className="text-5xl lg:text-7xl mt-2 sm:pt-0 tracking-wide">
-              {ltsEvent.event}
-            </p>
-            <p className="text-3xl lg:text-5xl tracking-wide py-4">
-              {ltsEvent.date}
-            </p>
-            <button className="text-2xl tracking-wider border-2 py-2 px-6 cursor-pointer font-semibold hover:text-gray-900 hover:bg-white">
-              MORE
-            </button>
+      {dataState
+        .slice(0)
+        .reverse()
+        .slice(0, 3)
+        .map((ltsEvent) => (
+          <div className="md:flex px-2 my-4 py-8">
+            <img
+              src={ltsEvent.image}
+              alt="/"
+              className="w-full h-56 md:w-[235px] md:h-[235px] border-2"
+            />
+            <div className="text-white md:px-8 my-auto">
+              <p className="text-5xl lg:text-7xl mt-2 sm:pt-0 tracking-wide">
+                {ltsEvent.event}
+              </p>
+              <p className="text-3xl lg:text-5xl tracking-wide py-4">
+                {ltsEvent.date}
+              </p>
+              <button className="text-2xl tracking-wider border-2 py-2 px-6 cursor-pointer font-semibold hover:text-gray-900 hover:bg-white">
+                MORE
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
