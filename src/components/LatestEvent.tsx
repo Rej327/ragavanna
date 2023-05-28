adc99ee0b335c862df08713b4a03b5de1d55cf74
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-05-28 12:58:10 +0800

    Update

--- src/components/LatestEvent.tsx
+++ src/components/LatestEvent.tsx
@@ -30,41 +30,43 @@ export default function LatestEvents() {
     <div id="event" className="w-full bg-[#292e33] px-4 py-8">
       <h1 className=" text-white text-4xl md:text-7xl">Latest Events</h1>
       {dataState.length <= 0 && <Loader />}
-      {dataState
-        .slice(0, 3) // Limit the number of elements
-        .reverse() // Reverse the array
-        .map((ltsEvent) => (
-          <div key={ltsEvent.id} className="md:flex px-2 my-4 py-8">
-            <Image
-              key={ltsEvent.id}
-              src={ltsEvent.image}
-              alt="/"
-              width={235}
-              style={{ objectFit: "cover" }}
-              height={235}
-              className=" w-[235px] h-[235px] mx-auto sm:mx-0 border-2"
-            />
-            <div className="text-white md:px-8 my-auto">
-              <p
-                key={ltsEvent.id}
-                className="text-5xl lg:text-7xl mt-2 sm:pt-0 tracking-wide"
-              >
-                {ltsEvent.event}
-              </p>
-              <p
-                key={ltsEvent.id}
-                className="text-3xl lg:text-5xl tracking-wide py-4"
-              >
-                {ltsEvent.date}
-              </p>
-              <Link href="/allEvents">
-                <button className="text-2xl tracking-wider border-2 py-2 px-6 cursor-pointer font-semibold hover:text-gray-900 hover:bg-white">
-                  MORE
-                </button>
-              </Link>
-            </div>
-          </div>
-        ))}
+      {Array.isArray(dataState)
+        ? dataState
+            .slice(0, 3) // Limit the number of elements
+            .reverse() // Reverse the array
+            .map((ltsEvent) => (
+              <div key={ltsEvent.id} className="md:flex px-2 my-4 py-8">
+                <Image
+                  key={ltsEvent.id}
+                  src={ltsEvent.image}
+                  alt="/"
+                  width={235}
+                  style={{ objectFit: "cover" }}
+                  height={235}
+                  className=" w-[235px] h-[235px] mx-auto sm:mx-0 border-2"
+                />
+                <div className="text-white md:px-8 my-auto">
+                  <p
+                    key={ltsEvent.id}
+                    className="text-5xl lg:text-7xl mt-2 sm:pt-0 tracking-wide"
+                  >
+                    {ltsEvent.event}
+                  </p>
+                  <p
+                    key={ltsEvent.id}
+                    className="text-3xl lg:text-5xl tracking-wide py-4"
+                  >
+                    {ltsEvent.date}
+                  </p>
+                  <Link href="/allEvents">
+                    <button className="text-2xl tracking-wider border-2 py-2 px-6 cursor-pointer font-semibold hover:text-gray-900 hover:bg-white">
+                      MORE
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))
+        : null}
     </div>
   );
 }
